489183150b104512d965bf0bd74368d9f58c64b7
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-07-26 13:58:56 -0700

    added grid and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from './components/Square'
 
 class App extends Component{
   constructor(props){
@@ -13,6 +14,11 @@ class App extends Component{
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div id="gameboard">
+          {this.state.board.map((val, idx) => {
+            return <Square key={idx} value={val} />
+          })}
+        </div>
       </>
     )
   }
